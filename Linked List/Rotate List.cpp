615d46e25bef946d2e247b09504b673aad89efca
Aatmik_Mishra <aatmik2006@gmail.com>
2026-09-08 20:01:17 +0530

    Create Rotate List.cpp

---
+++ Linked List/Rotate List.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0) return head;
+
+        //find length and last node
+        int n = 1;
+        ListNode* tail = head;
+        while (tail->next) {
+            tail = tail->next;
+            n++;
+        }
+
+        // reduce k
+        k = k % n;
+        if (k == 0) return head;
+
+        tail->next = head;
+
+        int steps = n - k;
+        ListNode* newTail = head;
+        while (--steps) {
+            newTail = newTail->next;
+        }
+
+        // break circle
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+
+        return newHead;
+    }
+};
